6a8b6dbec6832855598491e0c248d4450410ca0b
MichaelHDesigns <devilking6105@gmail.com>
2023-07-23 20:13:07 -0400

    it's a me web3

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,11 +1,11 @@
 import { w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { configureChains, createConfig } from 'wagmi'
-import { goerli, mainnet } from 'wagmi/chains'
+import { polygon } from 'wagmi/chains'
 
 export const walletConnectProjectId = 'd675701473f6801dbf1b6abc57dadaf7'
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, ...(import.meta.env?.MODE === 'development' ? [goerli] : [])],
+  [polygon, ...(import.meta.env?.MODE === 'development' ? [polygon] : [])],
   [w3mProvider({ projectId: walletConnectProjectId })],
 )
 
